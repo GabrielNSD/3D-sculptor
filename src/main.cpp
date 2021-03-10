407912ede243a73a644c2fc9281d7d2d4319a4b0
gabrielnsd <gabrielnsdantas@gmail.com>
2021-03-10 08:12:46 -0300

    setVoxel, putVoxel and cutVoxel working

--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,11 @@
 #include "Sculptor.hpp"
 
 int main() {
-    Sculptor teste(2,2,2);
+    Sculptor teste(3,3,3);
     teste.setColor(1,1,1,0.7);
     teste.putVoxel(0,0,0);
+    teste.setColor(0,1,1,0.7);
+    teste.putVoxel(2,0,0);
     teste.writeOFF((char*)"teste.off");
 
     //std::cout << "Hello Easy C++ project!" << std::endl;
